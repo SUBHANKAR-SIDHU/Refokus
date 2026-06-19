eec957a94cbf9dd28a4c2fc902b26f4bc1ac65de
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-19 19:27:54 +0530

    embbed the button components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import Button from './Button'
 
 function Navbar() {
     return (
-        <div className='max-w-5xl mx-auto'>
-            <nav className='flex gap-30 p-5 items-center text-sm font-extralight'>
-                <img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
-                <div className='flex gap-10 '>
-                    {["Home", "Work", "Contact", "", "Vlog"].map((elem, index) => (
-                        <div>
-                            {elem == 0 && <h1 className='h-6 border-r text-zinc-600 w-0.5'></h1>}
-                            {elem}
-                            {elem == 1 ? <h1 className='w-1 h-1 bg-green-400 backdrop-brightness-95 rounded-full'>lfa</h1> : ""}
-                        </div>
-                    ))}
+        <div className='max-w-4xl mx-auto'>
+            <nav className='flex gap-30 py-4 justify-between items-center text-sm font-extralight'>
+                <div className='flex gap-16 '>
+                    <img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
+                    <div className='flex gap-10  '>
+                        {["Home", "Work", "Culture", "", "News"].map((elem, index) => (
+                            <div >
+                                <a className='flex items-center gap-1' href="#">
+                                    {elem == 0 && <h1 className='h-4 mt-1 border-r text-zinc-600 w-0.5'></h1>}
+                                    {index == 1 ? <span style={{ boxShadow: "0 0 0.25em #00FF19" }} className='inline-block w-1 h-1 bg-green-400 backdrop-brightness-95 rounded-full'></span> : ""}
+                                    {elem}
+                                </a>
+                            </div>
+                        ))}
+                    </div>
                 </div>
+                <Button />
             </nav>
             <hr className='text-zinc-600 w-full ' />
         </div>
