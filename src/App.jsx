983588f18c6927b78b0423766b308a9ecac0926d
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-19 19:21:02 +0530

    embbed the works components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Works from './components/Works'
 
 function App() {
   return (
     <div className='w-full text-white h-screen bg-zinc-900'>
       <Navbar/>
+      <Works/>
     </div>
   )
 }
