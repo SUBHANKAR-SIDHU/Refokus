57511457f5e0b1988fbeae2c3def9cf42cf3d025
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-20 21:06:56 +0530

    change vlog to news

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
                     <img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
                     <div className='flex gap-10  '>
                         {["Home", "Work", "Culture", "", "News"].map((elem, index) => (
-                            <div >
+                            <div key={index}>
                                 <a className='flex items-center gap-1' href="#">
                                     {elem == 0 && <h1 className='h-4 mt-1 border-r text-zinc-600 w-0.5'></h1>}
                                     {index == 1 ? <span style={{ boxShadow: "0 0 0.25em #00FF19" }} className='inline-block w-1 h-1 bg-green-400 backdrop-brightness-95 rounded-full'></span> : ""}
