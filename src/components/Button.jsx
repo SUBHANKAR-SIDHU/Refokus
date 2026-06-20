0dbe3da37752d7f21146e3a13edd612af249b729
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-20 21:05:18 +0530

    add title props and minor button adjustment

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { PiArrowBendDownRightThin } from "react-icons/pi";
 
-function Button() {
+function Button({title="Start a Project"}) {
     return (
-        <div className='flex items-center  gap-2 rounded-full px-3 py-1.5 text-black bg-zinc-200 '>
-            <h1>Start a Project</h1>
-            <span className='mt-0.5'><PiArrowBendDownRightThin/></span>
+        <div className='max-w-33.75 flex justify-between items-center gap-2 rounded-full h-7 text-black bg-zinc-200 '>
+            <h1 className='ml-2' >{title}</h1>
+            <span className='mt-0.5 mr-1'><PiArrowBendDownRightThin/></span>
         </div>
     )
 }
