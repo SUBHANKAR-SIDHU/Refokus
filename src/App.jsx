f643a3e2db0e1c6190a38cd947235a1ddbe8ef72
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-20 21:17:43 +0530

    add strips,products,marquees routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Works from './components/Works'
+import Strips from './components/Strips'
+import Products from './components/Products'
+import Marquees from './components/Marquees'
 
 function App() {
   return (
-    <div className='w-full text-white h-screen bg-zinc-900'>
+    <div className='w-full text-white  bg-zinc-900'>
       <Navbar/>
       <Works/>
+      <Stripss/>
+      <Products/>
+      <Marquees/>
     </div>
   )
 }
