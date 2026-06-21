c7224114be807beab6c31e33cf94d7c10916f5b8
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-21 13:33:59 +0530

    add cards and footer routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Works from './components/Works'
 import Strips from './components/Strips'
 import Products from './components/Products'
 import Marquees from './components/Marquees'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className='w-full text-white  bg-zinc-900'>
+    <div className='w-full text-white  bg-zinc-900 '>
       <Navbar/>
       <Works/>
       <Strips/>
       <Products/>
       <Marquees/>
+      <Cards/>
+      <Footer/>
     </div>
   )
 }
