f2c8a8f8eb04bcc65f18d19db622a1d0844a5a1d
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-23 21:28:53 +0530

    add diraction props and motion framer

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,15 +1,26 @@
+import { motion } from 'motion/react'
 import React from 'react'
 
-function Marquee({data}) {
-    
+function Marquee({ data, diraction }) {
+
+
     return (
-        <div className='flex w-full py-5 gap-25 '>
-            {data.map((url,idx)=>(
-                <img className='w-[8vw]' src={url} alt="" />
-            ))}
-            {data.map((url,idx)=>(
-                <img className='w-[8vw]' src={url} alt="" />
-            ))}
+        <div className="flex w-full overflow-hidden">
+            <motion.div initial={{ x: diraction === "left" ? "0" : "-100%" }} animate={{ x: diraction === "left" ? "-100%" : "0" }} transition={{ ease: "linear", duration: 15 ,repeat:Infinity }}
+                className="flex shrink-0 gap-20 py-8 pr-24"
+            >
+                {data.map((url, index) => (
+                    <img key={index} src={url} className="w-[8vw]" />
+                ))}
+            </motion.div>
+            <motion.div initial={{ x: diraction === "left" ? "0" : "-100%" }} animate={{ x: diraction === "left" ? "-100%" : "0" }} transition={{ ease: "linear", duration: 15 ,repeat:Infinity }}
+                className="flex shrink-0 gap-20 py-8 pr-24"
+            >
+                {data.map((url, index) => (
+                    <img key={index} src={url} className="w-[8vw]" />
+                ))}
+            </motion.div>
+
         </div>
     )
 }
