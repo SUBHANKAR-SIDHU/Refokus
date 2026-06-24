325adc620eca3e03d6f77e1d8693b6da98f39731
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-24 19:07:41 +0530

    pass the hover props

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,7 @@ function Cards() {
     return (
         <div className='max-w-4xl mx-auto flex gap-1 mt-15'>
             <Card width={"basis-1/3"} start={false} para={true}/>
-            <Card width={"basis-2/3"} start={true} para={false} hover={"bg-violet-600"}/>
+            <Card width={"basis-2/3"} start={true} para={false} hover="true"/>
         </div>
     )
 }
